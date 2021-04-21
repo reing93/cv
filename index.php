6bcd741de433857b5ca9442311d36bc0ba4a8de0
REING <giangtra92@gmail.com>
2021-04-21 13:36:10 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIANG-CV</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="./assets/css/base.css">
+	<link rel="stylesheet" href="./assets/css/main.css">
+	
+    <link rel="stylesheet" href="./assets/fonts/fontawesome-free-5.13.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;400;600;900&display=swap" rel="stylesheet">
+    
+</head>
+
+<body>
+    <div class="app">
+        <header class="header" >
+		   
+				<nav class="header_navbar">
+					<ul class="header_navbar-list">
+						<li class="header_navbar-item">
+							<a href="#私" class="logo">18TE442</a>
+						</li>
+					</ul>
+	
+					<ul class="header_navbar-list">
+						
+						<li class="header_navbar-item">
+							<a href="#私" >私</a>
+						</li>
+						<li class="header_navbar-item">
+							<a href="#趣味" >趣味</a>
+						</li>
+						<li class="header_navbar-item">
+							<a href="#履歴" >履歴</a>
+						</li>
+						<li class="header_navbar-item">
+							<a href="#卒業研究" >卒業研究</a>
+						</li>
+						<li class="header_navbar-item">
+							<a href="#お問い合わせ" >お問い合わせ</a>
+						</li>
+					</ul>
+					<!-- <button class="toggle-menu" aria-label="Responsive Navigation Menu">☰</button> -->
+	
+				</nav>
+			
+		</div>
+
+
+
+		</header>
+
+		<!-- MAIN -->
+
+		<div class="main">
+
+			<!-- START: ABOUT -->
+		<section class="section section-about" id="私">
+			<div class="container">
+				<div class="row justify-content-center align-items-center">
+
+					<div class="flex1">
+						<div class="section-about--img wow fadeInUp">
+							<img class="img-fluid mx-auto d-block" src="assets/img/IMG_0578.JPG" alt="About Picture">
+						</div>
+					</div>
+					
+					<div class="flex2">
+						<div class="about-desc">
+							<div class="about-desc-content text-left">
+								<h2 style="font-size: 40px;">About Me</h2>
+								<p style="font-size: 17px;">第一工業大学 工学部 情報電子システム学科 の３年生 A クラス 18TE442  グエンチャ ザンです。</p>
+								<p style="font-size: 17px;">Web作りのが好きですので将来に頑張ってWeb devoloperになりたいです。</p>
+							</div>
+
+							<ul class="about-desc-social text-left my-4 list-unstyled list-inline">
+		                        <li class="list-inline-item"><a class="hover-state" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a></li>
+		                        <li class="list-inline-item"><a class="hover-state" href="https://www.instagram.com/?hl=ja" target="_blank"><i class="fab fa-instagram"></i></a></li>
+								<li class="list-inline-item"><a class="hover-state" href="https://twitter.com/home" target="_blank"><i class="fab fa-twitter"></i></a></li>
+								<li class="list-inline-item"><a class="hover-state" href="https://www.skype.com/ja/" target="_blank"><i class="fab fa-skype"></i></a></li>
+								
+								
+		                    </ul>
+
+							<div class="about-desc-more text-left">
+								
+									<div class="cot1">
+
+										<div class="col-lg-6">
+											<div class="about-desc-info">
+												<b>Age : </b>
+												<span>26</span>
+											</div>
+										</div>
+										<div class="col-lg-6">
+											<div class="about-desc-info">
+												<b>Location : </b>
+												<span>Japan,Tokyo</span>
+												
+											</div>
+										</div>
+									</div>
+									
+									<div class="cot2">
+									
+									    <div class="col-lg-6">
+										    <div class="about-desc-info">
+											    <b>Phone : </b>
+											    <a href="tel:08084248599">080-8424-8599</a>
+										    </div>
+									<div class="col-lg-6">
+										    <div class="about-desc-info">
+											    <b>Mail : </b>
+											    <a href="mailto:18TE442@ditu.jp">18TE442@ditu.jp</a>
+										    </div>
+									    </div>
+											
+									</div>	
+									
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END: ABOUT -->
+
+
+		<!-- start-sở thích -->
+
+		<section class="section section-dark section-services" id="趣味">
+			<div class="container">
+				<div class="section-head">
+					<h2>My Hobbies</h2>
+				 </div>
+				<div class="tang1">
+					<div class=" guitar col-sm-4">
+					  <img src="assets/img/guitar.jpg" alt="guitar" class="guitar_img image">
+					  <div class="color"></div>
+					  <div class="pourcolor">
+						<h3>GUITAR</h3>
+						<span>好きな曲をカバーしながらギターを弾く</span>	  
+					  </div>
+					</div>
+					<div class=" reading col-sm-4">
+					  <img src="assets/img/docsach.jpeg" alt="reading" class="reading_img image">
+					  <div class="color"></div>
+					  <div class="pourcolor">
+						<h3>BOOK</h3>  
+						<span>本のジャンルの歴史、探偵、科学</span></div>
+					</div>
+					<div class=" swimming col-sm-4">
+					  <img src="assets/img/boiloi.jpeg" alt="swimming" class="swimming_img image">
+					  <div class="color"></div>
+					  <div class="pourcolor">
+						<h3>SWIMMING</h3>  
+						<span>水泳による運動さっぱりやっている</span>
+					  </div>
+					</div>
+				  </div>
+				  <div class="tang2">
+		
+					<div class=" travelling col-sm-4">
+					  <img src="assets/img/dulich.jpg" alt="travelling" class="travelling_img image">
+					  <div class="color"></div>
+					  <div class="pourcolor">
+						<h3>TRAVELLING</h3>  
+						<span>お母さんと一緒に楽しんで旅行している</span></div>
+					</div>
+					<div class="cooking col-sm-4">
+					  <img src="assets/img/cooking.jpg" alt="cooking" class="cooking_img image">
+					  <div class="color"></div>
+					  <div class="pourcolor">
+						<h3>COOKING</h3>  
+						<span>暇なときベトナム料理を作っている</span></div>
+					</div>
+					<div class="watching col-sm-4">
+					  <img src="assets/img/watching.jpeg" alt="thu_phap" class="watching_img image">
+					  <div class="color"></div>
+					  <div class="pourcolor">
+						<h3>WATCHING</h3>  
+						<span>ホラーの映画を一人で見ている</span></div>
+					</div>
+		
+				  </div> 
+				 
+			</div>
+		</section>
+			
+		<!--END: sở thích -->
+
+
+		<!-- START: QUÁ TRÌNH HỌC TẬP -->
+
+		<section class="section  section-services" id="履歴">
+			<div class="container">
+				<div class="section-head">
+					<h2>履歴</h2>
+				</div>
+				<div class="history">
+					<h3 class="hoc"><i class="fas fa-graduation-cap"></i>学歴</h3>
+					<table class="tb1">
+						<tr>
+							<td><b>POSTS AND TELECOMMUNICATIONS INSTITUTE OF TECHNOLOGY</b></td>
+							<td>2011/9 - 2015/10</td>
+						</tr>
+						<tr>
+							<td>経営管理</td>
+							
+						</tr>
+						<tr>
+							<td><b>飛鳥学院　日本語学校</b></td>
+							<td>2016/4 - 2018/3</td>
+						</tr>
+						<tr>
+							<td>日本語を勉強したり日本文化を研究したりしてる。</td>
+						</tr>
+						<tr>
+							<td><b>第一工業大学</b></td>
+							<td>2018/4 - 2022/3</td>
+						</tr>
+						<tr>
+							<td>工学部情報電子システム学科</td>
+						</tr>
+						<tr>
+							<td>PHP, JavaScript, HTML, CSS, SQLなどなどの専門技術</td>
+						</tr>
+						<tr>
+							<td>日本簿記: 商業簿記と工業簿記</td>
+						</tr>
+					</table>
+					<h3 class="hoc"> <i class="fas fa-trophy"></i>免許.資格</h3>
+					<table class="tb2">
+						<tr>
+							<td>日本語能力試験 N2</td>
+						</tr>
+						<tr>
+							<td>DATABASE N2</td>
+						</tr>
+					</table>
+					
+			
+				</div>
+			</div>
+		</section>
+
+		<!-- QUÁ TRÌNH HỌC TẬP- END -->
+
+
+		<!-- nghiên cứu -start -->
+
+		<section class="section section-dark section-services" id="卒業研究">
+			<div class="container">
+				<div class="section-head">
+					<h2>卒業研究</h2>
+				
+				<div class="nghiencuu">
+					<div class="anh anh1"></div>
+					<div class="anh anh2"></div>
+					<div class="anh anh3"></div>
+				</div>
+				
+				</div>
+			</div>
+		</section>
+
+
+		<!-- nghiên cứu- end -->
+
+		<!-- contact-start -->
+
+		<!-- START: CONTACT -->
+		<section class="section section-contact" id="お問い合わせ">
+			<div class="container">
+				<div class="section-head">
+					<h2>お問い合わせ</h2>
+				</div>
+			</div>
+			<div class="to">
+				<div class="form">
+					<h2 style="font-size: 30px;">Contact Form</h2>
+					<label style="margin-left: -380px;">名前</label>
+					<input type="text" name="hoten">
+					<label style="margin-left: -350px;">電話番号</label>
+					<input type="text" name="sdt">    
+					<label style="margin-left: -370px;">メール</label>
+					<input type="text" name="email"> 
+					<input id="submit" type="submit" name="submit" value="Send">
+				</div>                
+			</div>
+		</section>
+		<!-- END: CONTACT -->
+	</div>
+</body>
+</html>
+					
+					
+
+
+	
+
+
+	
+
+
+
+
+			
+	
+		
+
+
+
+
+        
+
+        
